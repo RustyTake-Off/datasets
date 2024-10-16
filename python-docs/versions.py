60cf14064766dea5211cc516b21a4a53ce547bbe
RustyTake-Off <109174317+RustyTake-Off@users.noreply.github.com>
2024-10-17 01:08:12 +0200

    Add last_checked date

--- python-docs/versions.py
+++ python-docs/versions.py
@@ -20,12 +20,13 @@ def extract_python_doc_info(
         url (str): URL of the documentation page
         details (dict[str, Any]): Metadata about the version
     """
+
     try:
         response = requests.get(url)
         response.raise_for_status()
         soup = BeautifulSoup(response.content, "html.parser")
 
-        # Extract the last update date
+        # Extract last update date
         p_tag = soup.find("p")
 
         if p_tag is not None:
@@ -38,6 +39,7 @@ def extract_python_doc_info(
             last_updated = cleanup_date(last_updated)
             last_update_date = datetime.fromisoformat(last_updated)
             one_year_ago = datetime.now() - timedelta(days=ONE_YEAR_DAYS)
+            time_now = datetime.now().date().isoformat()
 
             if last_update_date < one_year_ago:
                 details["skip"] += 1
@@ -46,6 +48,7 @@ def extract_python_doc_info(
                 )
 
                 return {
+                    "last_checked": time_now,
                     "last_update": details["last_update"],
                     "plain_text_link": details["plain_text_link"],
                     "specific": details["specific"],
@@ -82,6 +85,7 @@ def extract_python_doc_info(
         )
 
         return {
+            "last_checked": time_now,
             "last_update": last_updated,
             "plain_text_link": plain_text_link,
             "specific": specific_version,
@@ -89,38 +93,42 @@ def extract_python_doc_info(
 
     except Exception as e:
         print(f"Error processing version {version}: {str(e)}")
-        # Increment the skip counter if it's not already True
+
+        # Increment skip counter if lower than MAX_SKIP_COUNT
         if details["skip"] < MAX_SKIP_COUNT:
             details["skip"] += 1
             print(f'Increasing skip counter to {details['skip']}')
 
         return {
+            "last_checked": time_now,
             "last_update": details["last_update"],
             "plain_text_link": details["plain_text_link"],
             "specific": details["specific"],
         }
 
 
-# Load the versions data from the YAML file
-data = load_versions(VERSIONS_FILE)
+if __name__ == "__main__":
+    # Load versions data from the YAML file
+    data = load_versions(VERSIONS_FILE)
 
-for version, details in data["versions"].items():
-    # Remove leading zeros from the version to construct the URL
-    major, minor = version.split(".")
-    clean_version = f"{int(major)}.{int(minor)}"
+    for version, details in data["versions"].items():
+        # Remove leading zeros from the version
+        major, minor = version.split(".")
+        clean_version = f"{int(major)}.{int(minor)}"
 
-    # Skip version if skip counter reaches MAX_SKIP_COUNT
-    if details["skip"] >= MAX_SKIP_COUNT:
-        print(f"Skipping version {clean_version}")
-        continue
+        # Skip version if skip counter reaches MAX_SKIP_COUNT
+        if details["skip"] >= MAX_SKIP_COUNT:
+            print(f"Skipping version {clean_version}")
+            continue
 
-    url = data["url"].format(version=clean_version)
-    info = extract_python_doc_info(clean_version, url, details)
+        url = data["url"].format(version=clean_version)
+        info = extract_python_doc_info(clean_version, url, details)
 
-    # Update the YAML data with the extracted information
-    details["last_update"] = info["last_update"]
-    details["plain_text_link"] = info["plain_text_link"]
-    details["specific"] = info["specific"]
+        # Update YAML data with extracted information
+        details["last_checked"] = info["last_checked"]
+        details["last_update"] = info["last_update"]
+        details["plain_text_link"] = info["plain_text_link"]
+        details["specific"] = info["specific"]
 
-# Write the updated data back to the YAML file
-update_versions(VERSIONS_FILE, data)
+    # Write updated data back to the YAML file
+    update_versions(VERSIONS_FILE, data)
